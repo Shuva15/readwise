2dc9b61e30fcbc785f643d718297f8123d525db9
Shuva15 <shuvajitmahanta02@gmail.com>
2023-10-27 18:53:08 +0530

    delete didn't needed files

---
+++ frontend/src/components/FileUpload.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+
+const FileUpload = () => {
+  return (
+    <div>
+        <h2>Upload File</h2>
+        <input type="file" accept=".pdf" />
+        
+    </div>
+  );
+};
+
+export default FileUpload;
